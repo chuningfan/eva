5dcd292e148ca4fd953acdc7ed8cff96418008ed
Vic.Chu <ningfan.chu@activenetwork.com>
2019-03-05 17:10:38 +0800

    remove Spring dependency

--- eva-core/src/main/java/eva/core/base/config/ServerConfig.java
+++ eva-core/src/main/java/eva/core/base/config/ServerConfig.java
@@ -2,31 +2,18 @@ package eva.core.base.config;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-
 import eva.core.base.ResourceProvider;
 
-@PropertySource("classpath:eva.properties")
 public class ServerConfig {
 	
-	@Value("${eva.server.id}")
 	private String serverId = UUID.randomUUID().toString();
-	@Value("${eva.server.port}")
 	private int port = 8763;
-	@Value("${eva.server.connector.size}")
 	private int bossSize = 1;
-	@Value("${eva.server.processor.size}")
 	private int workerSize = Runtime.getRuntime().availableProcessors();
-	@Value("${eva.server.enable.monitor}")
 	private boolean needMonitor;
-	@Value("${eva.server.enable.asyncProcessing}")
 	private boolean asyncProcessing;
-	@Value("${eva.server.async.queue.size}")
 	private int asyncQueueSize = 30;
-	@Value("${eva.registry.address}")
 	private String registryAddress;
-	@Value("$(eva.server.timeout)")
 	private int serverTimeoutSec = -1;
 	private volatile ResourceProvider provider;
 	
